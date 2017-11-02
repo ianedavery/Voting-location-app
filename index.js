1e3f794446494253c8f806e4e6c0a34aa0f35de2
Ian Avery <ianedavery@gmail.com>
2017-11-02 16:26:38 -0700

    added geocoding function

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const CIVIC_SEARCH_URL = 'https://www.googleapis.com/civicinfo/v2/voterinfo';
-const GEOCODING_URL = 'https://maps.googleapis.com/maps/api/geocode';
+const GEOCODING_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 function getDataFromGeocodingApi(searchTerm, callback) {
 	console.log('Geocoding API queried');
 	const query = {
-		key: 'AIzaSyCc83loc2gllyDhzsjFtTs7ueurzLuU_8U',
-		address: `${searchTerm}`
+		address: `${searchTerm}`,
+		key: 'AIzaSyCc83loc2gllyDhzsjFtTs7ueurzLuU_8U'
 	}
 	$.getJSON(GEOCODING_URL, query, callback);
 }
@@ -19,15 +19,8 @@ function getDataFromCivicApi(searchTerm, callback) {
 	$.getJSON(CIVIC_SEARCH_URL, query, callback);
 }
 
-/*function translateToCoordinates (data) {
-	const coordinateArray = data.pollingLocations.map(item => {
-		data.pollingLocations.push(`https://maps.googleapis.com/maps/api/geocode/json?address=${item}&key=AIzaSyCc83loc2gllyDhzsjFtTs7ueurzLuU_8U`);
-
-	});
-	console.log(coordinateArray);
-}*/
-
 function displayGoogleVoterInfoResults(data) {
+	console.log(data);
 	console.log('display function ran');
 	let locations = data.pollingLocations;
 	let locationArray = [];
@@ -35,22 +28,15 @@ function displayGoogleVoterInfoResults(data) {
 		locationArray.push(data.pollingLocations[i].address.line1 + '\ ' + data.pollingLocations[i].address.city + '\ ' + data.pollingLocations[i].address.state + '\ ' + data.pollingLocations[i].address.zip);
 	}
 	console.log(locationArray);
-	/*let coordinatesArray = [];
-	for (let i = 0; i < locationArray.length; i++) {
-		coordinatesArray.push(`https://maps.googleapis.com/maps/api/geocode/json?address=${locationArray[i]}&key=AIzaSyCc83loc2gllyDhzsjFtTs7ueurzLuU_8U`);
+	const coordinatesArray = [];
+	for(let i = 0; i < locations.length; i++) {
+		getDataFromGeocodingApi(locationArray[i], displayCoordinateResults);
 	}
-	console.log(coordinatesArray);*/
-	//}
-	//let locationArray = [];
-	//for(let i = 0; i < data.length; i++) {
+}
 
-	//}
-	//data.pollingLocations.map((item) => 
-		//({locationName: item.address}));
-		//console.log(item);
-	//const coordinateArray = [results.address];
-	//console.log(locationArray);
-	//console.log(results);
+function displayCoordinateResults(data) {
+	const coordinatesArray = [(data.results[0].geometry.location.lat + '\ ' + data.results[0].geometry.location.lng)];
+	console.log(coordinatesArray);
 }
 
 function watchSubmit() {
