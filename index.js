29f5cd6250b3a140876cd0bd0bf05cc644cc9bed
Ian Avery <ianedavery@gmail.com>
2017-11-04 12:48:12 -0700

    added marker info

--- index.js
+++ index.js
@@ -19,30 +19,39 @@ function getDataFromCivicApi(searchTerm, callback) {
 	$.getJSON(CIVIC_SEARCH_URL, query, callback);
 }
 
-function initMap(latitude, longitude) {
+function renderPollingLocationInfo(locationName) {
+	return '<stong>' + locationName + '<strong>';
+}
+
+function initMap(x, y, z) {
 	let myLatLng = {lat: latitude, lng: longitude};
 	let map = new google.maps.Map(document.getElementById('map'), {
-		center: {
-			lat: latitude, lng: longitude
-		},
+		center: myLatLng,
 		zoom: 17
 	});
 	let marker = new google.maps.Marker({
         map: map,
         position: myLatLng,
-        title: 'Hello World!'
+        title: ''
     });
+    let infowindow = new google.maps.InfoWindow({});
+    google.maps.event.addListener(marker, 'click', (function (marker) {
+		return function () {
+			infowindow.setContent(address);
+			infowindow.open(map, marker);
+		}
+	})(marker));
 }
 
-
 function displayGoogleVoterInfoResults(data) {
-	console.log(data);
+	console.log(data.pollingLocations[0].address.locationName);
 	console.log('display function ran');
 	let locations = data.pollingLocations;
 	let locationArray = [];
 	for(let i = 0; i < locations.length; i++) {
 		locationArray.push(data.pollingLocations[i].address.line1 + '\ ' + data.pollingLocations[i].address.city + '\ ' + data.pollingLocations[i].address.state + '\ ' + data.pollingLocations[i].address.zip);
 	}
+	//renderPollingLocationInfo(`${data.pollingLocations[0].address.locationName}`);
 	console.log(locationArray);
 	locationArray.map(item => {
 		getDataFromGeocodingApi(item, displayCoordinateResults);
@@ -50,33 +59,35 @@ function displayGoogleVoterInfoResults(data) {
 }
 
 function displayCoordinateResults(data) {
-	//console.log(data.results[0].geometry.location.lat);
+	console.log(data.results[0].formatted_address);
 	latitude = data.results[0].geometry.location.lat;
 	longitude = data.results[0].geometry.location.lng;
-	initMap(latitude, longitude);
+	address = data.results[0].formatted_address;
+	initMap(latitude, longitude, address);
 }
-
 
 function watchSubmit() {
 	$('#address-form').submit(event => {
 		event.preventDefault();
 		console.log('enter button clicked');
-		const streetAddressTarget = $(event.currentTarget).find('#street-address');
-		const streetAddress = streetAddressTarget.val();
-		const cityTarget = $(event.currentTarget).find('#city');
-		const city = cityTarget.val();
-		const stateTarget = $(event.currentTarget).find('#state');
-		const state = stateTarget.val();
-		const zipTarget = $(event.currentTarget).find('#zip');
-		const zip = zipTarget.val();
-		const address = streetAddress + '\ ' + city + '\ ' + state + '\ ' + zip;
+		let streetAddressTarget = $(event.currentTarget).find('#street-address');
+		let streetAddress = streetAddressTarget.val();
+		let cityTarget = $(event.currentTarget).find('#city');
+		let city = cityTarget.val();
+		let stateTarget = $(event.currentTarget).find('#state');
+		let state = stateTarget.val();
+		let zipTarget = $(event.currentTarget).find('#zip');
+		let zip = zipTarget.val();
+		let address = streetAddress + '\ ' + city + '\ ' + state + '\ ' + zip;
 		console.log(address);
 		streetAddressTarget.val('');
 		cityTarget.val('');
 		stateTarget.val('');
 		zipTarget.val('');
 		getDataFromCivicApi(address, displayGoogleVoterInfoResults);
+		address = undefined;
 	});
 }
 
 $(watchSubmit);
+
