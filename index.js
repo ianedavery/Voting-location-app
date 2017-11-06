9d9dec7ef530ffb91602f495bf8328fcf3c279e1
Ian Avery <ianedavery@gmail.com>
2017-11-05 23:47:05 -0700

    cleaned up

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const CIVIC_SEARCH_URL = 'https://www.googleapis.com/civicinfo/v2/voterinfo';
 const GEOCODING_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
-let resultsArray = [];
+let coordinatesArray = [];
+let longAddressArray = [];
+let formattedAddressArray = [];
 
 function getDataFromGeocodingApi(searchTerm, callback) {
 	console.log('Geocoding API queried');
@@ -28,70 +30,43 @@ function renderSearchForm() {
 
 function handleSearchAnotherAddressClicks() {
 	$('#go-back').on('click', event => {
-		console.log('go back button clicked');
-		resultsArray = [];
+		coordinatesArray = [];
+		longAddressArray = [];
+		formattedAddressArray = [];
 		renderSearchForm();
 	});
 }
 
-function initMap(latitude, longitude, address, longAddress) {
-	//let myLatLng = [{lat: 37.541644, lng: -77.433904}, {lat: 37.543269, lng: -77.436210}];
-	//let myLatLng = {lat: latitude, lng: longitude};
-	//console.log(myLatLng);
-	let myLatLng = resultsArray;
+function initMap() {
+	let myLatLng = coordinatesArray;
+	let longAddress = longAddressArray;
+	let formattedAddress = formattedAddressArray;
 	let map = new google.maps.Map(document.getElementById('map'), {
 		center: myLatLng[0],
 		zoom: 8
 	});
 	let marker;
+	let infowindow = new google.maps.InfoWindow({});
 	for(let i=0; i<myLatLng.length; i++) {
 		marker = new google.maps.Marker({
         	map: map,
         	position: new google.maps.LatLng(myLatLng[i]),
-        	//title: ''
     	});
+   		google.maps.event.addListener(marker, 'click', (function (marker) {
+			return function () {
+				infowindow.setContent(longAddress[i] + `</br><a href="https://www.google.com/maps/place/${formattedAddress[i]}" target='_blank'>Directions</a>`);
+				infowindow.open(map, marker);
+			}
+		})(marker));
 	}
-    let infowindow = new google.maps.InfoWindow({});
-    google.maps.event.addListener(marker, 'click', (function (marker) {
-		return function () {
-			infowindow.setContent(longAddress + `</br><a href="https://www.google.com/maps/place/${address}">Directions</a>`);
-			infowindow.open(map, marker);
-		}
-	})(marker));
 }
 
-/*function initMap(latitude, longitude, address, longAddress) {
-	let myLatLng = {lat: latitude, lng: longitude};
-	let map = new google.maps.Map(document.getElementById('map'), {
-		center: myLatLng,
-		zoom: 17
-	});
-	let marker = new google.maps.Marker({
-        map: map,
-        position: myLatLng,
-        title: ''
-    });
-    let infowindow = new google.maps.InfoWindow({});
-    google.maps.event.addListener(marker, 'click', (function (marker) {
-		return function () {
-			infowindow.setContent(longAddress + `</br><a href="https://www.google.com/maps/place/${address}">Directions</a>`);
-			infowindow.open(map, marker);
-		}
-	})(marker));
-}*/
-
 function displayGoogleVoterInfoResults(data) {
-	let locationNames = data.pollingLocations;
-	for(let i = 0; i < locationNames.length; i++){
-		console.log(data.pollingLocations[i].address.locationName);
-	}0
-	console.log('display function ran');
 	let locations = data.pollingLocations;
 	let locationArray = [];
 	for(let i = 0; i < locations.length; i++) {
 		locationArray.push(data.pollingLocations[i].address.line1 + '\ ' + data.pollingLocations[i].address.city + '\ ' + data.pollingLocations[i].address.state + '\ ' + data.pollingLocations[i].address.zip);
 	}
-	console.log(locationArray);
 	let coordinatesArray = [];
 	locationArray.map(item => {
 		getDataFromGeocodingApi(item, displayCoordinateResults);
@@ -99,14 +74,10 @@ function displayGoogleVoterInfoResults(data) {
 }
 
 function displayCoordinateResults(data) {
-	resultsArray.push({lat: data.results[0].geometry.location.lat, lng: data.results[0].geometry.location.lng});
-	console.log(resultsArray);
-	console.log(data);
-	latitude = data.results[0].geometry.location.lat;
-	longitude = data.results[0].geometry.location.lng;
-	address = data.results[0].formatted_address;
-	longAddress = data.results[0].address_components[0].long_name + '\ ' + data.results[0].address_components[1].short_name;
-	initMap(latitude, longitude, address, longAddress);
+	coordinatesArray.push({lat: data.results[0].geometry.location.lat, lng: data.results[0].geometry.location.lng});
+	longAddressArray.push(data.results[0].address_components[0].long_name + '\ ' + data.results[0].address_components[1].short_name);
+	formattedAddressArray.push(data.results[0].formatted_address);
+	initMap();
 }
 
 function renderMap() {
@@ -129,7 +100,6 @@ function watchSubmit() {
 		let zipTarget = $(event.currentTarget).find('#zip');
 		let zip = zipTarget.val();
 		let address = streetAddress + '\ ' + city + '\ ' + state + '\ ' + zip;
-		console.log(address);
 		streetAddressTarget.val('');
 		cityTarget.val('');
 		stateTarget.val('');
